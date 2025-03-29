c2eba3054c66f2831a87a5a3a6d8f8584243445f
gineslopez11 <glopezpena@udesa.edu.ar>
2025-03-29 17:38:17 -0300

    agregue el .h

--- Ej1.cpp
+++ Ej1.cpp
@@ -1,58 +1,7 @@
 #include <iostream>
-#include <string>
-#include <iomanip>
+#include "Ej1.h"
+
 using namespace std;
-
-class Tiempo{
-    private:
-           int horas;
-           int minutos;
-           int segundos;
-           string am_pm;
-
-    public:
-           Tiempo (int h = 0, int m = 0, int s = 0, string ap = "a.m"){
-                if (h > 12 || m >= 60 || s >= 60){throw invalid_argument ("ERROR: estas utilizando valores fuera de rango");}
-                horas = h;
-                minutos = m;
-                segundos = s;
-                am_pm = ap;
-           }
-           ~Tiempo(){}
-
-           int getHoras(){ return horas; }
-           int getMinutos(){ return minutos; }
-           int getSegundos(){ return segundos; }
-           string getAm_Pm() {return am_pm; }
-           
-           void setHoras(int h){ horas = h; }
-           void setMinutos(int m){ minutos = m; }
-           void setSegundos(int s){ segundos = s; }
-           void setAm_Pm(string ap) {am_pm = ap; }
-
-           void imprimir24hs (){
-                int horas24 = horas;
-                if (horas24 == 12 && am_pm == "a.m"){
-                    horas24 = 0;
-                }
-                else if(am_pm == "p.m" && horas24 < 12){
-                    horas24 += 12;
-                }
-
-                cout <<setw(2)<<setfill('0')<<horas24<<"hh, "<<setw(2)<<setfill('0')
-                     <<setw(2)<<setfill('0')<<minutos<<"mm, "<<setw(2)<<setfill('0')
-                     <<setw(2)<<setfill('0')<<segundos<<"ss"<<setw(2)<<setfill('0')<<endl;
-           }
-           
-           void imprimir12hs(){
-            cout <<setw(2)<<setfill('0')<<horas<<"hh, "<<setw(2)<<setfill('0')
-                 <<setw(2)<<setfill('0')<<minutos<<"mm, "<<setw(2)<<setfill('0')
-                 <<setw(2)<<setfill('0')<<segundos<<"ss "<<setw(2)<<setfill('0')
-                 <<am_pm<<endl;
-                 
-           }
-};
-
 int main (){
     int h,m,s;
     string ap;
