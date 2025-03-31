f5ee6a238b3340e57db91acaf1f9cd6c8c323cc9
gineslopez11 <glopezpena@udesa.edu.ar>
2025-03-31 16:50:08 -0300

    La clase Banco ya esta

---
+++ EJ4/Banco.h
@@ -0,0 +1,38 @@
+#ifndef BANCO_H
+#define BANCO_H
+
+#include <string>
+#include <memory>
+#include <iostream>
+
+using namespace std;
+
+struct info{
+    int balance;
+    string tipo;
+    string nombre;
+};
+
+class Banco{
+    protected:
+        double balance;
+        string titularCuenta;
+    
+    public:
+        Banco (double bal = 0, string titular = ""){
+            balance = bal;
+            titularCuenta = titular; 
+        }
+
+        virtual double retirar(double cantidad) const = 0;
+        virtual vector<info> mostrarInfo() const = 0;
+
+        virtual void depositar(double cantidad){
+            balance += cantidad;
+        }
+
+
+
+};
+
+#endif
